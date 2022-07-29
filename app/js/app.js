82127a7bf699c9d69ed87512b2d596e3b8497173
Artem <nikolloteslla@gmail.com>
2022-07-29 19:57:53 +0300

    add  pages
    1)experts-all
    2)product-card
    3)products
    4) service-catalog
    5) service-category

--- app/js/app.js
+++ app/js/app.js
@@ -111,32 +111,51 @@ $(function () {
     slidesToScroll: 1,
     infinite: true,
     responsive: [{
-      breakpoint: 1200,
-      settings: {
-        centerMode: false,
-      }
-    },
-    {
-      breakpoint: 701,
-      settings: {
-        slidesToShow: 2,
-        arrows: false,
-        centerMode: false,
-      }
-    },
-    {
-      breakpoint: 469,
-      settings: {
-        slidesToShow: 1,
-        arrows: false,
-        centerMode: false,
-      }
-    },
-  ]
+        breakpoint: 1200,
+        settings: {
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 701,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          centerMode: false,
+        }
+      },
+      {
+        breakpoint: 469,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          centerMode: false,
+        }
+      },
+    ]
+  });
+
+  $('.review-slider').slick({
+    prevArrow: '<button class="next"><svg xmlns="http://www.w3.org/2000/svg" width="20.01" height="19.503" viewBox="0 0 20.01 19.503"><path id="arrow-right-solid" d="M8.508,38.956l.991-.991a1.067,1.067,0,0,1,1.514,0l8.682,8.677a1.067,1.067,0,0,1,0,1.514l-8.682,8.682a1.067,1.067,0,0,1-1.514,0l-.991-.991a1.073,1.073,0,0,1,.018-1.532l5.381-5.127H1.072A1.069,1.069,0,0,1,0,48.116V46.687a1.069,1.069,0,0,1,1.072-1.072H13.907L8.525,40.488A1.065,1.065,0,0,1,8.508,38.956Z" transform="translate(0 -37.65)" fill="#944ca2"/></svg></button>',
+    nextArrow: '<button class="prev"><svg xmlns="http://www.w3.org/2000/svg" width="20.01" height="19.503" viewBox="0 0 20.01 19.503"><path id="arrow-right-solid" d="M8.508,38.956l.991-.991a1.067,1.067,0,0,1,1.514,0l8.682,8.677a1.067,1.067,0,0,1,0,1.514l-8.682,8.682a1.067,1.067,0,0,1-1.514,0l-.991-.991a1.073,1.073,0,0,1,.018-1.532l5.381-5.127H1.072A1.069,1.069,0,0,1,0,48.116V46.687a1.069,1.069,0,0,1,1.072-1.072H13.907L8.525,40.488A1.065,1.065,0,0,1,8.508,38.956Z" transform="translate(0 -37.65)" fill="#944ca2"/></svg></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    responsive: [{
+        breakpoint: 700,
+        settings: {
+          arrows: false,
+        }
+      },
+
+    ]
   });
 
   $(".subscribe__box").on("click", function () {
     $(".subscribe__form").toggleClass("active");
+  });
+  $(".leave-review__box").on("click", function () {
+    $(".leave-review__form").toggleClass("active");
   });
 
   $(".header__menu-btn").on("click", function () {
@@ -151,6 +170,26 @@ $(function () {
     $(".slick-slider").slick("refresh");
     $(".slick-slider").slick("setPosition");
   }
+
+  if ($('.slider-card').length > 0) {
+    $('.slider-card__top').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      centerMode: true,
+      asNavFor: '.slider-card__bottom',
+    });
+    $('.slider-card__bottom').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      asNavFor: '.slider-card__top',
+      dots: false,
+      arrows: false,
+      focusOnSelect: true,
+    });
+  }
+
 });
 
 var clock;
