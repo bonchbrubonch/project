537b713cd1d3c324abc8c8d34ef99827f25070a6
Artem <nikolloteslla@gmail.com>
2022-07-30 14:21:20 +0300

    add pages
    1) about
    2) articl
    3) news

--- app/js/app.js
+++ app/js/app.js
@@ -97,6 +97,42 @@ $(function () {
     ]
   });
 
+  $('.product-all-slider').slick({
+    prevArrow: '<button class="next"><svg xmlns="http://www.w3.org/2000/svg" width="20.01" height="19.503" viewBox="0 0 20.01 19.503"><path id="arrow-right-solid" d="M8.508,38.956l.991-.991a1.067,1.067,0,0,1,1.514,0l8.682,8.677a1.067,1.067,0,0,1,0,1.514l-8.682,8.682a1.067,1.067,0,0,1-1.514,0l-.991-.991a1.073,1.073,0,0,1,.018-1.532l5.381-5.127H1.072A1.069,1.069,0,0,1,0,48.116V46.687a1.069,1.069,0,0,1,1.072-1.072H13.907L8.525,40.488A1.065,1.065,0,0,1,8.508,38.956Z" transform="translate(0 -37.65)" fill="#944ca2"/></svg></button>',
+    nextArrow: '<button class="prev"><svg xmlns="http://www.w3.org/2000/svg" width="20.01" height="19.503" viewBox="0 0 20.01 19.503"><path id="arrow-right-solid" d="M8.508,38.956l.991-.991a1.067,1.067,0,0,1,1.514,0l8.682,8.677a1.067,1.067,0,0,1,0,1.514l-8.682,8.682a1.067,1.067,0,0,1-1.514,0l-.991-.991a1.073,1.073,0,0,1,.018-1.532l5.381-5.127H1.072A1.069,1.069,0,0,1,0,48.116V46.687a1.069,1.069,0,0,1,1.072-1.072H13.907L8.525,40.488A1.065,1.065,0,0,1,8.508,38.956Z" transform="translate(0 -37.65)" fill="#944ca2"/></svg></button>',
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 701,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
 
   $(".product-card__like").on("click", function () {
     $(this).toggleClass("active");
@@ -189,6 +225,47 @@ $(function () {
       focusOnSelect: true,
     });
   }
+
+  $(".form").validate({
+    errorElement: "span",
+    rules: {
+      full_name: {
+        required: true,
+        minlength: 3
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      phone: {
+        required: true,
+      },
+      review: {
+        minlength: 5,
+        required: true,
+      },
+
+    },
+    messages: {
+      full_name: {
+        required: 'Введите имя*'
+      },
+      email: {
+        required: 'Введите почту*'
+      },
+      phone: {
+        required: 'Введите номер телефона*'
+      },
+      review: {
+        required: 'Оставьте отзыв*'
+      },
+    }
+  });
+
+
+
+
+
 
 });
 
